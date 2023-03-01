a82cc83cac27480576c6fd056c379c9fd4ac1f6d
Cindrom1 <cindrof@gmail.com>
2023-03-01 23:23:14 +0300

    add Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99)
+    {
+        int third_digit = Third_Digit(number);
+        Console.WriteLine($"Третья цифра числа {number} - {third_digit}");
+    }
+else
+    Console.WriteLine("Третьей цифры нет"); 
+
+
+int Third_Digit(int num)
+{
+    while(number > 999)
+    {
+        number = number / 10;
+    }
+    int result = number % 10;
+    return result;
+}
+
