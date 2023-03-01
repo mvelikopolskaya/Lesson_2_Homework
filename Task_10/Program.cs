d44cffddaf14bcebb70d92ad013e07a934ddb412
Cindrom1 <cindrof@gmail.com>
2023-03-01 22:12:53 +0300

    add Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 100 && number < 999)
+    {int second_digit = Second_Digit(number);
+    Console.WriteLine($"Вторая цифра числа {number} - {second_digit}");
+    }
+else
+    Console.WriteLine("Ошибка ввода"); 
+
+
+    int Second_Digit(int num)
+    {
+        int result = number / 10 % 10;;
+        return result;
+    }
